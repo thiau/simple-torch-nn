a5498067a6a11fb0f31185bbb9f47bff4ff9ad74
Thiago Araujo <thirauj@br.ibm.com>
2019-03-21 10:51:28 -0300

    create training helper

---
+++ app/helpers/training.py
@@ -0,0 +1,10 @@
+def train(model, input_data, labels, criterion, optimizer):
+    epochs = 5000
+    losses = list()
+    for epoch in range(epochs):
+        y_pred = model.forward(input_data)
+        loss = criterion(y_pred, labels)
+        losses.append(loss.item())
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
