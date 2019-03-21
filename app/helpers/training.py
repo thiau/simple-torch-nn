25d950f8a1d305224aa31bcca6d87469b0f477c1
Thiago Araujo <thirauj@br.ibm.com>
2019-03-21 10:51:41 -0300

    update loop variable

--- app/helpers/training.py
+++ app/helpers/training.py
@@ -1,7 +1,7 @@
 def train(model, input_data, labels, criterion, optimizer):
     epochs = 5000
     losses = list()
-    for epoch in range(epochs):
+    for _ in range(epochs):
         y_pred = model.forward(input_data)
         loss = criterion(y_pred, labels)
         losses.append(loss.item())
